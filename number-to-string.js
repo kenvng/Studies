ce557bfe2b1955f0ff528442511c84fd8f3c087a
vuken <kenvng@gmail.com>
2016-01-25 23:49:06 -0500

    Number to string

---
+++ number-to-string.js
@@ -0,0 +1,19 @@
+var n = 128;
+n = n.toString();
+console.log(n);
+
+/*
+var n = 256;
+n = n.toString();
+
+## The challenge:
+
+Create a file named number-to-string.js.
+
+In that file define a variable named n that references the number 128;
+
+Call the .toString() method on the n variable.
+
+Use console.log() to print the results of the .toString() method to the
+terminal.
+*/
