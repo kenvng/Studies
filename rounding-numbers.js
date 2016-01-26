f560d6ae285d2ecd063912f21a70d2d81edac694
vuken <kenvng@gmail.com>
2016-01-25 23:43:29 -0500

    Rounding numbers

---
+++ rounding-numbers.js
@@ -0,0 +1,26 @@
+var roundUp = 1.5;    // define a variable named roundUp that references the float 1.5.
+Math.round(0);      // use the Math.round() method to round the number up.
+                      // This method rounds either up or down to the nearest integer.
+var rounded = Math.round(roundUp);  //  Define a second variable named rounded
+                                    //that references the output of the Math.round() method,
+                                    //passing in the roundUp variable as the argument.
+console.log(rounded); // Use console.log() to print that number to the terminal.
+/*
+We can do basic math using familiar operators like +, -, *, /, and %.
+For more complex math, we can use the Math object.
+In this challenge we'll use the Math object to round numbers.
+
+## The challenge:
+Create a file named rounding-numbers.js.
+1) In that file define a variable named roundUp that references the float
+1.5.
+2) We will use the Math.round() method to round the number up. This method
+rounds either up or down to the nearest integer.
+An example of using Math.round():
+Math.round(0.5);
+3) Define a second variable named rounded that references the output of the
+Math.round() method, passing in the roundUp variable as the argument.
+4) Use console.log() to print that number to the terminal.
+Check to see if your program is correct by running this command:
+Javascripting verify rounding-numbers.js
+*/
