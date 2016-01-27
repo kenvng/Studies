1dcd8d83af1906404057cacd72d9ebd875966c7e
vuken <kenvng@gmail.com>
2016-01-26 22:53:28 -0500

    Arrays

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+console.log(pizzaToppings);
+
+/*
+An array is a list of values. Here's an example:
+
+   var pets = ['cat', 'dog', 'rat'];
+
+### The challenge:
+
+Create a file named arrays.js.
+
+In that file define a variable named pizzaToppings that references an
+array that contains three strings in this order: tomato sauce, cheese,
+pepperoni.
+
+Use console.log() to print the pizzaToppings array to the terminal.
+
+Check to see if your program is correct by running this command:
+ */
